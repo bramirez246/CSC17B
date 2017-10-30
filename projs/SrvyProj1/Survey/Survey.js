567b063697ae95d47056b91035f49c4d4033a010
bramirez246 <bramirez246@gmail.com>
2017-10-30 12:57:45 -0700

    Add files via upload

---
+++ projs/SrvyProj1/Survey/Survey.js
@@ -0,0 +1,15 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function Survey(title, description, qstnNum){
+    title="Music Survey";                                                                                       //Variable that holds title of survey
+    dscrptn="This survey is about music preferences.";                                 //Variable that holds description
+
+
+};
+
+//Declaring object of type Survey
+var survey = new Survey();
+
