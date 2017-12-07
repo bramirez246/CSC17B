61b208d8429d330922da13655dd79df006a45f87
bramirez246 <bramirez246@gmail.com>
2017-12-07 15:30:32 -0800

    Add files via upload

---
+++ projs/GrpProj3/GrpProj3/Skills.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+function Skill(name, description, cost){
+    this.skillName="";
+    this.killDscrptn="";
+    this.skillCost=0;
+    
+    this.skillName = name;
+    this.skillDscrptn = description;
+    this.skillCost = cost;
+};
